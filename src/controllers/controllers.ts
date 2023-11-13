4736ad46635f0bb7a604b4fe90f761fef2662707
Franco Agustin Cola Ibañez <C733341@quilmes.com.ar>
2023-11-13 16:04:34 -0300

    login y register

--- src/controllers/controllers.ts
+++ src/controllers/controllers.ts
@@ -1,71 +1,99 @@
 import { Request, Response } from "express";
-import { Users } from '../models/users';
+import { Users } from "../models/users";
 
-
-const users: Users[]=[]
+const users: Users[] = [];
 
 export const register = (req: Request, res: Response) => {
-    const {email, password} = req.body
-    if (email == null || password == null ){
-        return res.status(400).json({
-            msg: "El email y la contraseña son obligatorios"
-        })
+  const { email, password } = req.body;
+  if (email == null || password == null) {
+    return res.status(400).json({
+      msg: "El email y la contraseña son obligatorios",
+    });
+  }
+  let existUser = false;
+  users.forEach((e) => {
+    if (email == e.email) {
+      existUser = true;
+      return;
     }
-    let existUser = false;
-    users.forEach(e => {
-        if(email == e.email){
-            existUser = true
-            return
-        }
-    })
-    if(existUser){
-        return res.status(400).json({
-            msg: `email ${email} ya en uso`
-        })
+  });
+  if (existUser) {
+    return res.status(400).json({
+      msg: `email ${email} ya en uso`,
+    });
+  }
+  const user = new Users(email, password);
+
+  users.push(user);
+  res.status(201).json({
+    msg: "Usuario creado",
+    user,
+  });
+};
+
+export const login = (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  if (email == null || password == null) {
+    return res.status(400).json({
+      msg: "El email y la contraseña son obligatorios",
+    });
+  }
+  let user: Users | null = null;
+
+  for (const userInUsers of users) {
+    if (userInUsers.email == email) {
+      user = userInUsers;
+      break;
     }
-    const user = new Users(email,password);
+  }
 
-    users.push(user)
-    res.status(201).json({
-        msg: "Usuario creado", user
-    }) 
-    
-}
+  if (user == null)
+    return res.json({
+      msg: "Usuario no encontrado",
+    });
 
+  if (password != user.password) {
+    return res.status(400).json({
+      msg: "Contraseña incorrecta",
+    });
+  }
 
-
-
+  return res.json({
+    msg: "Sesion iniciada",
+    user,
+  });
+};
 
 export const getAllProducts = (_: Request, res: Response) => {
-    console.table(users)
-    const products = [
-        {
-            nombre : "Auto",
-            precio : 800,
-            caracteristicas : "anda",
-        },
-        {
-            nombre : "moto",
-            precio : 800,
-            caracteristicas : "anda",
-        },
-        {
-            nombre : "camion",
-            precio : 800,
-            caracteristicas : "anda",
-        },
-        {
-            nombre : "avion",
-            precio : 800,
-            caracteristicas : "anda",
-        },
-        {
-            nombre : "barco",
-            precio : 800,
-            caracteristicas : "anda",
-        }
-    ]
-    res.json({
-        products
-    })
-} 
+  console.table(users);
+  const products = [
+    {
+      nombre: "Auto",
+      precio: 800,
+      caracteristicas: "anda",
+    },
+    {
+      nombre: "moto",
+      precio: 800,
+      caracteristicas: "anda",
+    },
+    {
+      nombre: "camion",
+      precio: 800,
+      caracteristicas: "anda",
+    },
+    {
+      nombre: "avion",
+      precio: 800,
+      caracteristicas: "anda",
+    },
+    {
+      nombre: "barco",
+      precio: 800,
+      caracteristicas: "anda",
+    },
+  ];
+  res.json({
+    products,
+  });
+};
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { getAllProducts, register } from "../controllers/controllers";
+import { getAllProducts, register, login } from "../controllers/controllers";
 
 const router = Router()
 
 router.get("/products", getAllProducts)
 router.post("/users", register)
+router.post ("/login", login)
 
 
 export default router
